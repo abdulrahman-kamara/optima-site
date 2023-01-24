fd79873ca924ee3dc8bacb0282cc44453c7c2f39
abdulrahman-kamara <rahmankamara@icloud.com>
2023-01-24 17:19:59 +0100

    second réseau commit

--- src/pages/Réseau.js
+++ src/pages/Réseau.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from "react";
+import { NavLink } from "react-router-dom";
 import supervisionService from "../Context/SupervisionService"
+import Checkbox from '@mui/material/Checkbox';
 
 const Réseau = () =>{
   // Tous les adherents
@@ -35,6 +37,14 @@ const handleChange = (event) => {
   const inputValue = event.target.value;
   setSearch(inputValue);
 };
+const toggleFiltre = () => {
+  setFilterOpened(!filterOpened);
+};
+
+const handleCheck = () => {
+  setActif(!actif);
+};
+
 
 
 
@@ -56,6 +66,41 @@ const handleChange = (event) => {
                   autoComplete="off"
                 />
         </div>
+        <span className="filtre">
+                <NavLink className="link" to="#" onClick={toggleFiltre}>
+                  <span>{actif ? "Actif" : "Tous"}</span>
+                  <i
+                    className={
+                      "fas fa-angle-up " + (filterOpened ? "reverse-icon" : "")
+                    }
+                  ></i>
+                </NavLink>
+                <div
+                  className={
+                    "c-dropdown-list " + (filterOpened ? "c-show" : "")
+                  }
+                >
+                  <ul>
+                    <li>
+                      <Checkbox
+                        value={4}
+                        checked={actif}
+                        onChange={handleCheck}
+                        inputProps={{
+                          "aria-label": "primary checkbox",
+                        }}
+                      />
+                      Actif
+                    </li>
+                  </ul>
+                </div>
+              </span>
+              <div className="aderent-list">
+
+                <ul>
+                  <li></li>
+                </ul>
+              </div>
       </div>
     </div>
    </div>

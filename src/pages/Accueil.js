f901978b72f0cb3a145286d058556864c82a5539
kunta-kante <rahmankamara@icloud.com>
2023-01-31 09:28:47 +0100

    SWR package install pour fetcher le donner

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -3,7 +3,43 @@ import React from "react";
 function Accueil() {
   return (
     <section className="hero">
-      <h1>Welcome to nav</h1>
+      <div
+        className="hero-title"
+        style={{
+          display: "block",
+          fontSize: "24px",
+          fontweight: "bold",
+          textAlign: "center",
+          marginBottom: "20px",
+        }}
+      >
+        <h2>Offre Accompagnement Qualité :</h2>
+        <a href="https://github.com/">
+          {" "}
+          Profiter de l'expérience de notre pôle auditeur !
+        </a>
+        <ul>
+          <li>
+            {" "}
+            Audit interne ou Audit blanc Formation, mise en place de système
+            Qualité.{" "}
+          </li>
+          <li> Préparation audit de certification ou accréditation.</li>
+          <li> ISO 9001 / ISO 17020 / ISO 14001 </li>
+          <li>
+            {" "}
+            Document Unique d'Evaluation des Risques / Certification MASE{" "}
+          </li>
+          <li> Conseil et Expertise en métrologie légale.</li>
+        </ul>
+        <a href="http://">
+          Contactez nous ! Nous vous aiderons à définir vos besoins
+          04.42.50.96.90 / contact@cercleoptima.com
+        </a>
+      </div>
+      <main>
+        <p>This is main</p>
+      </main>
     </section>
   );
 }
--- src/pages/Nore-reseau/Map/AdherentMap.js
+++ src/pages/Nore-reseau/Map/AdherentMap.js
@@ -1,65 +1,73 @@
-import React from 'react'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
-import { useState } from 'react';
-import {  Icon } from 'leaflet';
-import "leaflet/dist/leaflet.css"
-import data from '../../../data/skateboard-parks.json'
+import React from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useState } from "react";
+import { Icon } from "leaflet";
+import "leaflet/dist/leaflet.css";
+import data from "../../../data/skateboard-parks.json";
+// import supervisionService from "../../../Context/SupervisionService"
+// import useSWR from 'swr'
 
+export const icon = new Icon({
+  iconUrl: "/skateboarding.svg",
+  iconSize: [25, 25],
+});
 
-export const icon  = new Icon({
-    iconUrl : "/skateboarding.svg",
-    iconSize : [25, 25]
-})
+// const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 export default function AdherentMap() {
-    const [aderentLoc, setAdherentLoc] = useState(null)
-  
+  const [aderentLoc, setAdherentLoc] = useState(null);
+  //     const [actif, setActif] = useState(null)
+
+  // const url = supervisionService.getAllAdherent()
+
+  // const {data, error} = useSWR(url, fetcher)
+
+  // const locationadhrent = data && !error ?  data.slice(0, 500) : [];
+
   return (
-    <MapContainer center={[45.4, -75.7]} zoom={13} scrollWheelZoom={false}
-    style={{width:'50%', height:'50vh'}}
+    <MapContainer
+      center={[45.4, -75.7]}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{ width: "50%", height: "50vh" }}
     >
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    {data.features.map((park) => (
-     <Marker
-     key={park.properties.PARK_ID}
-     position={[
-        park.geometry.coordinates[1],
-        park.geometry.coordinates[0]
-     ]}
-   eventHandlers={{
-    click: () => {
-        setAdherentLoc(park)
-    }
-   }}
-    icon={icon}
-     />
-     ))}
-     {aderentLoc && (
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {data.features.map((park) => (
+        <Marker
+          key={park.properties.PARK_ID}
+          position={[
+            park.geometry.coordinates[1],
+            park.geometry.coordinates[0],
+          ]}
+          eventHandlers={{
+            click: () => {
+              setAdherentLoc(park);
+            },
+          }}
+          icon={icon}
+        />
+      ))}
+      {aderentLoc && (
         <Popup
-        position={[
+          position={[
             aderentLoc.geometry.coordinates[1],
-            aderentLoc.geometry.coordinates[0]
-         ]}
-         onClose={()=>{
-            setAdherentLoc(null)
-         }}
-
-         
+            aderentLoc.geometry.coordinates[0],
+          ]}
+          onClose={() => {
+            setAdherentLoc(null);
+          }}
         >
-           
-            <div>
-                <h2>{aderentLoc.properties.NAME}</h2>
-                <p>{aderentLoc.properties.DESCRIPTIO}</p>
-            </div>
+          <div>
+            <h2>{aderentLoc.properties.NAME}</h2>
+            <p>{aderentLoc.properties.DESCRIPTIO}</p>
+          </div>
         </Popup>
-     )}
-    
-
-  </MapContainer>
-  )
+      )}
+    </MapContainer>
+  );
 }
 
 //   <span className="filtre">
